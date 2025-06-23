c8f20e2a809abab098b1902a3d06c8c9520fc6b0
Mikiejoe <mjosephomondi@gmail.com>
2025-06-23 16:28:24 +0300

    add fonts and images

--- pages/accomodations.vue
+++ pages/accomodations.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <!-- Features -->
-    <div class="max-w-[100vw] m-2">
+    <div class="max-w-[100vw] m-2 normal">
       <div class="px-2 sm:px-6 lg:px-2">
         <div
           class="relative h-120 md:h-[90dvh] aspect-w-16 aspect-h-7 flex flex-col bg-[url('/assets/img/accomodations.jpg')] bg-cover bg-center bg-no-repeat rounded-2xl overflow-hidden">
@@ -10,28 +10,28 @@
           <div class="absolute inset-0 bg-gradient-to-tr from-black to-transparent z-10"></div>
 
           <!-- Content on top of gradient -->
-          <div class="relative z-20 mt-auto w-2/3 md:max-w-lg ps-5 pb-5 md:ps-10 md:pb-10">
-            <p class="text-white uppercase text-lg md:text-xl">Accomodations</p>
-            <h1 class="text-2xl sm:text-3xl lg:text-7xl  text-white">
+          <div class="relative z-20 mt-auto md:p-8 p-4 lg:px-16 2xl:p-20">
+            <p class="text-white uppercase text-xs lg:text-sm 2xl:text-[1.3rem]">Accomodations</p>
+            <h1 class="text-4xl sm:text-6xl md:text-4xl  lg:text-6xl 2xl:text-[5.8rem] text-white heading">
               Coming Soon
             </h1>
           </div>
 
         </div>
       </div>
-      <div class="md:p-8 p-4 2xl:p-12">
-        <div class="w-[60vw] my-8 mb-2 sm:mb-2 md:mb-2">
+      <div class="md:p-16 p-4 lg:px-16 2xl:px-20">
+        <div class="2xl:w-[70vw] lg:w-[60vw] md:w-[70vw] my-8 mb-2 sm:mb-2 md:mb-2">
 
-          <h2 class="font-awesome-serif w-full text-2xl sm:text-3xl lg:text-7xl text-neutral-900 font-semibold">
+          <h2 class="heading w-full text-4xl sm:text-6xl md:text-4xl  lg:text-6xl  2xl:text-[5.8rem] text-neutral-900 font-semibold">
             We are excited to soon offer group homes and day centres for our NDIS participants. </h2>
         </div>
 
         <!-- Blog Article -->
-        <div class="lg:max-w-1/2 px-4 pt-6 lg:pt-10 pb-12 sm:px-6 lg:px-8 mx-auto">
-          <div class="max-w-2xl">
+        <div class="lg:max-w-1/2 md:max-w-2/3 px-4 pt-6 lg:pt-10 pb-12 sm:px-6 lg:px-8 mx-auto text-xs lg:text-sm 2xl:text-[1.3rem] mb-28">
+          <div class="">
 
             <!-- Content -->
-            <div class="space-y-5 md:space-y-8 text-gray-800 text-md 2xl:text-lg">
+            <div class="space-y-5 md:space-y-8">
 
 
               <p class="">
@@ -68,18 +68,27 @@
             <!-- End Content -->
           </div>
         </div>
-        <div class="space-y-4 2xl:mb-16 mb-8">
-          <h1 class="font-semibold 2xl:text-[1.8rem]">UPCOMING HOMES</h1>
-          <div class="grid grid-cols-1  md:grid-cols-3 gap-4 h-full">
-            <div v-for="i in 3" class="h-[50vh">
-              <div class="w-full h-42 lg:h-64 2xl:h-[40vh] bg-gray-400 rounded-md"></div>
-              <div class="space-y-2 mt-2 2xl:text-[1.8rem] text-[0.9rem] md:text-[0.7rem] lg:text-[1rem] 2xl:space-y-4">
-                <div class="flex justify-between items-center">
-                  <p class="font-semibold">Canning Vale Shared Home</p>
-                  <p class="font-semibold">3 Bedrooms</p>
+        <div class="space-y-4 2xl:mb-16 mb-8 lg:text-lg 2xl:text-[1.8rem] text-md md:text-sm">
+          <h1 class="font-semibold text-[1.2rem] md:text-[1.3rem] 2xl:text-[1.8rem]">UPCOMING HOMES</h1>
+          <div class="grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 gap-4 2xl:gap-6 h-full">
+            <div v-for="home in homes">
+              <div class="w-full h-42 lg:h-64 2xl:h-[40vh]  bg-gray-100  rounded-xl overflow-clip ">
+                <img class="size-full object-cover object-center hover:scale-110 transition-all duration-300 cursor-pointer" :src="home.image" :alt="home.name">
+              </div>
+              <div class="space-y-4 mt-2 2xl:space-y-8">
+                <div class="space-y-2 2xl:space-y-3  ">
+                  <div class="lg:flex justify-between items-center gap-2">
+                    <p class="font-semibold text-[1.2rem] md:text-[1.3rem] 2xl:text-[1.8rem]">{{ home.name }}</p>
+                    <p class="font-semibold text-[1rem] md:text-[1.2rem] 2xl:text-[1.4rem]">{{ home.feature }}</p>
+                  </div>
+                  <div class="font-semibold text-[1rem] md:text-[1.2rem] 2xl:text-[1.4rem] flex items-center gap-1">
+                    <Icon name="mdi:map-marker-outline" />
+                    <p>{{ home.location }}</p>
+                  </div>
                 </div>
-                <p class="font-semibold 2xl:text-2xl">Canning Vale, WA</p>
-                <button class="rounded-md border w-full p-2 2xl:p-4 font-semibold outline-none">Coming Soon</button>
+                
+                <button class="rounded-md border w-full p-2 2xl:p-4 mt-4 font-semibold outline-none text-[1.2rem] md:text-[1.3rem] 2xl:text-[1.8rem]">Coming
+                  Soon</button>
               </div>
             </div>
 
@@ -92,3 +101,33 @@
     <!-- End Features -->
   </div>
 </template>
+
+
+<script setup lang="tsx">
+const homes = [
+  {
+    name: "Canning Vale Shared Home",
+    feature: "3 Bedrooms",
+    location: "Canning Vale, WA",
+    image: "/images/homes/homeinterior.jpg"
+  },
+  {
+    name: "Sunset Valley Retreat",
+    feature: "4 Bedrooms",
+    location: "Sunset Valley",
+    image: "/images/homes/outdoor.jpg"
+  },
+  {
+    name: "Riverbend Family House",
+    feature: "5 Bedrooms",
+    location: "Riverbend, TX",
+    image: "/images/homes/indoor.jpg"
+  },
+]
+
+
+const getImageUrl = (image: string) => {
+  return new URL(`~/assets/img/${image}`, import.meta.url).href
+}
+
+</script>
--- pages/ndis.vue
+++ pages/ndis.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
     <!-- Features -->
-    <div class="max-w-[100vw] m-2">
+    <div class="max-w-[100vw] m-2 normal">
       <div class="px-2 sm:px-6 lg:px-2">
         <div
           class="relative h-120 md:h-[90dvh] aspect-w-16 aspect-h-7 flex flex-col bg-[url('/assets/img/ndis.jpg')] bg-cover bg-center bg-no-repeat rounded-2xl overflow-hidden">
 
           <div class="absolute inset-0 bg-gradient-to-tr from-black to-transparent z-10"></div>
           <!-- Content on top of gradient -->
-          <div class="relative z-20 mt-auto w-2/3 md:max-w-lg ps-5 pb-5 md:ps-10 md:pb-10">
-            <p class="text-white uppercase text-lg md:text-2xl">learn</p>
-            <h1 class="text-2xl sm:text-3xl lg:text-7xl  text-white">
+          <div class="relative z-20 mt-auto md:p-8 p-4 lg:p-16 2xl:p-20">
+            <p class="text-white uppercase text-xs lg:text-sm 2xl:text-[1.3rem]">learn</p>
+            <h1 class="text-4xl sm:text-3xl md:text-4xl  lg:text-6xl 2xl:text-7xl heading text-white">
               What is NDIS?
             </h1>
           </div>
@@ -18,23 +18,23 @@
         </div>
       </div>
 
-      <div class="w-full px-4 sm:px-8 my-8 mb-2 sm:mb-2 md:mb-2">
+      <div class="w-full px-4 sm:px-8 my-8 2xl:px-20 lg:px-16 mb-2 sm:mb-2 md:mb-2 ">
 
-        <h2 class="font-awesome-serif w-full text-2xl sm:text-3xl lg:text-7xl text-neutral-900 font-semibold">
+        <h2 class="heading w-full text-4xl sm:text-3xl md:text-4xl lg:text-6xl  2xl:text-[5.8rem] text-neutral-900 font-semibold">
           The NDIS (National Disability Insurance Scheme) is the Australian Government’s program that provides funding
           for supports and services to people with permanent and significant disabilities.
         </h2>
       </div>
 
       <!-- Blog Article -->
-      <div class="max-w-3xl px-4 pt-6 lg:pt-10 pb-12 sm:px-6 lg:px-8 mx-auto">
-        <div class="max-w-2xl">
+      <div class="lg:max-w-1/2 md:max-w-2/3 px-4 pt-6 lg:pt-10 pb-12 sm:px-6 lg:px-8 mx-auto 2xl:mt-24 lg:mt-16 text-xs lg:text-sm 2xl:text-[1.3rem]">
+        <div class="">
 
           <!-- Content -->
           <div class="space-y-5 md:space-y-8">
 
 
-            <p class="text-md text-gray-800">
+            <p class=" text-gray-800">
               It helps eligible people get the funding they need for reasonable and necessary supports that improve
               independence and quality of life. Examples of NDIS-funded supports include personal care, therapy,
               equipment, housing assistance and skill development. </p>
@@ -44,24 +44,26 @@
           <!-- Content -->
           <div class="mt-5 md:space-y-2">
 
-            <h3 class="text-sm font-semibold text-gray-800 sm:text-base">
+            <h3 class="font-semibold text-gray-800">
               Who is eligible? You may be eligible for NDIS if you:
             </h3>
-
-            <p class="text-md text-gray-800">
-              . Have a permanent disability (such as intellectual, physical, sensory, neurological or psychosocial) that
-              means you need help in your daily life<br>
-              . Are under 65 years old. (Special provisions exist for younger children).<br>
-              . Are an Australian citizen, permanent resident, or have a Protected Special Category visa<br>
-              . Need support from people or equipment for everyday activities
-            </p>
+            <div class="pl-8 lg:pl-12">
+              <ul class=" text-gray-800 list-disc">
+                <li> Have a permanent disability (such as intellectual, physical, sensory, neurological or psychosocial)
+                  that
+                  means you need help in your daily life</li>
+                <li>Are under 65 years old. (Special provisions exist for younger children).</li>
+                <li> Are an Australian citizen, permanent resident, or have a Protected Special Category visa</li>
+                <li>Need support from people or equipment for everyday activities</li>
+              </ul>
+            </div>
           </div>
           <!-- End Content -->
 
           <!-- Content -->
           <div class="mt-5 md:space-y-2">
 
-            <h3 class="text-sm font-semibold text-gray-800 sm:text-base">
+            <h3 class=" font-semibold text-gray-800">
               You can check eligibility on the official NDIS website or by contacting your Local Area Coordinator (LAC)
             </h3>
           </div>
@@ -70,11 +72,11 @@
           <!-- Content -->
           <div class="mt-5 md:space-y-2">
 
-            <h3 class="text-sm font-semibold text-gray-800 sm:text-base">
+            <h3 class=" font-semibold text-gray-800">
               How does it works?
             </h3>
 
-            <p class="text-md text-gray-800">
+            <p class=" text-gray-800">
               Once accepted into the NDIS, you get an NDIS plan with funding tailored to your goals. You use that
               funding to pay for services (like ours). The NDIS is not means-tested – your income doesn’t matter – and
               the funding is personally allocated to you. </p>
@@ -84,14 +86,12 @@
           <!-- Content -->
           <div class="mt-5 md:space-y-2">
 
-            <h3 class="text-sm font-semibold text-gray-800 sm:text-base">
+            <h3 class=" font-semibold text-gray-800">
               How Taley helps:
             </h3>
-            <p class="text-xs font-semibold text-gray-800 sm:text-base">
+            <p class=" text-gray-800">
               Taley Disability Services is an NDIS-registered provider in Western Australia.
-            </p>
 
-            <p class="text-md text-gray-800">
               Our staff can walk you through understanding the NDIS. We explain how to apply, what documentation you
               need, and how to use your plan. For example, our Support Coordinators help participants “make the best use
               of their supports” in the plan.
